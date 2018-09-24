07678efe3c0c3fb95b11db1be2032dbdba217481
Curtis Getz <curtisgetz+git@gmail.com>
2018-09-24 14:41:46 -0400

    Update VideoFragment.java

--- app/src/main/java/com/example/android/bakingapp/VideoFragment.java
+++ app/src/main/java/com/example/android/bakingapp/VideoFragment.java
@@ -15,13 +15,18 @@ import android.widget.Toast;
 import com.example.android.bakingapp.model.Step;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -98,6 +103,10 @@ public class VideoFragment extends android.support.v4.app.Fragment {
                 step_org = savedInstanceState.getParcelable(STEP);
                 videoUrl = step_org.getVideoURL();
                 pos = stepArrayList.indexOf(step_org);
+                //todo Curtis - get position and state from savedInstanceState
+                currentPosition = savedInstanceState.getLong(POSITION);
+                state = savedInstanceState.getBoolean(STATE);
+                Log.i("VideoFragment", String.valueOf(state));
 
                 description_text_view.setText(step_org.getDescription());
                 String v = savedInstanceState.getString(VIDEO);
@@ -152,8 +161,10 @@ public class VideoFragment extends android.support.v4.app.Fragment {
             next_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    simpleExoPlayer = null;
-//                    releasePlayer();
+                    //todo Curtis- clear player details (video urls and position) then release
+                    clearPlayerDetails();
+                   // simpleExoPlayer = null;
+                   releasePlayer();
                     if ( pos < stepArrayList.size() - 1 ) {
                         pos++;
                     } else {
@@ -183,6 +194,8 @@ public class VideoFragment extends android.support.v4.app.Fragment {
             prev_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    //todo Curtis- clear player details (video urls and position) then release
+                    clearPlayerDetails();
                     releasePlayer();
                     if ( pos < stepArrayList.size() - 1 ) {
                         pos--;
@@ -230,6 +243,16 @@ public class VideoFragment extends android.support.v4.app.Fragment {
 
 
 
+//TODO Added by Curtis for testing
+
+    private void clearPlayerDetails(){
+        video_url = null;
+        video_to_be_saved = null;
+        videoUrl = null;
+        currentPosition = C.TIME_UNSET;
+
+    }
+
 
     /**
      * Initialize ExoPlayer.
@@ -237,6 +260,8 @@ public class VideoFragment extends android.support.v4.app.Fragment {
      * @param videoUri The URI of the sample to play.
      */
     private void initializePlayer(Uri videoUri) {
+        //todo Curtis - release player so there aren't 2 instances
+        releasePlayer();
         if ( simpleExoPlayer == null ) {
             String uriString = videoUri.toString();
             if ( uriString.isEmpty() ) {
@@ -248,6 +273,49 @@ public class VideoFragment extends android.support.v4.app.Fragment {
                 if ( getContext() != null ) {
                     simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
                     simpleExoPlayerView.setPlayer(simpleExoPlayer);
+                    //TODo Curtis - player Listener to change state on play/pause.
+                    // getPlayWhenReady does not get the current state. It only gets the original
+                    // setPlayWhenReady value which will not change when player is paused/resumed.
+                    //So we do it manually here when the state changes
+                    simpleExoPlayer.addListener(new ExoPlayer.EventListener() {
+
+                        @Override
+                        public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+                        }
+
+                        @Override
+                        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+                        }
+
+                        @Override
+                        public void onLoadingChanged(boolean isLoading) {
+
+                        }
+
+                        @Override
+                        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                            if(playWhenReady && playbackState == ExoPlayer.STATE_READY) {
+                                state = true;
+                            }else if (!playWhenReady) {
+                                state = false;
+                            }
+                        }
+
+
+
+                        @Override
+                        public void onPlayerError(ExoPlaybackException error) {
+
+                        }
+
+                        @Override
+                        public void onPositionDiscontinuity() {
+
+                        }
+                    });
+
 
                     // Prepare the MediaSource.
                     String userAgent = Util.getUserAgent(getContext(), "BakingApp");
@@ -256,7 +324,9 @@ public class VideoFragment extends android.support.v4.app.Fragment {
                     if ( currentPosition != C.TIME_UNSET )
                     simpleExoPlayer.seekTo(currentPosition);
                     simpleExoPlayer.prepare(mediaSource);
-                    simpleExoPlayer.setPlayWhenReady(true);
+                    //TODO Curtis - use 'state' variable in setPlayWhenReady
+                    //simpleExoPlayer.setPlayWhenReady(true);
+                    simpleExoPlayer.setPlayWhenReady(state);
                 }
             }
         }
@@ -269,6 +339,7 @@ public class VideoFragment extends android.support.v4.app.Fragment {
             currentPosition = simpleExoPlayer.getCurrentPosition();
             Log.i("TAG", "onPause: " + currentPosition);
             state = simpleExoPlayer.getPlayWhenReady();
+
             simpleExoPlayer.stop();
             simpleExoPlayer.release();
             simpleExoPlayer = null;
@@ -294,6 +365,8 @@ public class VideoFragment extends android.support.v4.app.Fragment {
         if ( simpleExoPlayer != null ) {
             simpleExoPlayer.stop();
             simpleExoPlayer.release();
+            //todo Curtis set player to null
+            simpleExoPlayer = null;
         }
     }
 
@@ -337,10 +410,13 @@ public class VideoFragment extends android.support.v4.app.Fragment {
 
             Log.i("TAG", "onSaveInstanceState: " + currentPosition);
             currentState.putBoolean(STATE, state);
+            Log.i("VideoFragment", String.valueOf(state));
+
         }
         if ( video_url != null ) {
             currentState.putString("v_for_two", video_url);
         }
+      //  releasePlayer();
         super.onSaveInstanceState(currentState);
     }
 
